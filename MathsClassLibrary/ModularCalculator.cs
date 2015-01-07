f61069ce05e3559abf792daa604e1febde807e07
Lewis Johnson <lewisj489@gmail.com>
2015-01-07 18:58:15 +0000

    Initial Commit

---
+++ MathsClassLibrary/ModularCalculator.cs
@@ -0,0 +1,50 @@
+﻿/* *
+ * Created by Lewis Johnson.
+ * You can use this in anyway you want ! Fork on GitHub and add too the library.
+ * 
+ * This class is a simple calculator.
+ * 
+ * Example input: 5 * 4598 + 718 + 5 * 3988 * 2 + 52 + 365 * 46 % 4877
+ * 
+ * CAUTION: This is currently set up so % is the last operator. This is necessary in this calculator.
+ * 
+ * And it will return your answer.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MathsClassLibrary
+{
+    class ModularCalculator
+    {
+        public static string Input(int sum)
+        {
+            var inputData = new List<string>() {""};
+            while (inputData[0] != "%")
+            {
+                inputData = new List<string>(Console.ReadLine().Split(' ').ToList());
+                sum = Calculate(inputData[0], sum, Convert.ToInt32(inputData[1]));
+            }
+
+            sum = Calculate(inputData[0], sum, Convert.ToInt32(inputData[1]));
+            return sum.ToString();
+        }
+
+        private static int Calculate(string sign, int result, int operand)
+        {
+            switch (sign)
+            {
+                case "+": return result += operand; 
+                case "-": return result -= operand;
+                case "*": return result *= operand;
+                case "%": return result %= operand;
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ MathsClassLibrary/SimpleMaths.cs
@@ -0,0 +1,66 @@
+﻿/* *
+ * Created by Lewis Johnson.
+ * You can use this in anyway you want ! Fork on GitHub and add too the library.
+ * 
+ * This class is for simple maths functions.
+ * It is recommended that you turn on outlining.
+ */
+
+using System;
+
+namespace MathsClassLibrary
+{
+    public class SimpleMaths
+    {
+        public int APlusB(int a, int b)
+        {
+            var result = a + b;
+            return result;
+        }
+
+        public int ArithmicProgression(int a, int b, int c)
+        {
+            //First term        : a
+            //Common Differnce  : b
+            //Number of terms   : c
+            var result = 0;
+
+            for (var i = 1; i < c; i++)
+            {
+                a += b;
+                result += a;
+            }
+            return result;
+        }
+
+        public int MinOrMaxOfTwo(int a, int b, bool minimum)
+        {
+            return minimum ? Math.Min(a, b) : Math.Max(a, b);
+        }
+
+        public int MinOrMaxOfThree(int a, int b, int c, bool minimum)
+        {
+            return minimum ? Math.Min(Math.Min(a, b), c) : Math.Max(Math.Min(a, b), c);
+        }
+
+        public double RoundedDivision(double a, double b, double c, bool useThree)
+        {
+            return useThree ? Math.Round(a / b / c) : Math.Round(a/b);
+        }
+
+        public double FahrenheitToCelsius(double fahrenhiet, bool roundResult)
+        {
+            fahrenhiet -= 32;
+            fahrenhiet = fahrenhiet / 1.8;
+            return roundResult ? Math.Round(fahrenhiet) : fahrenhiet;
+        }
+
+        public double CelsiusToFahrenheit(double celsius, bool roundResult)
+        {
+            celsius += 32;
+            celsius = celsius * 1.8;
+
+            return roundResult ? Math.Round(celsius) : celsius;
+        }
+    }
+}
